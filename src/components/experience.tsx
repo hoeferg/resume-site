e3541a3a84006c91cc9b1acd70e9ace8274ff584
Gayle Hoefer <gaylehoefer@gmail.com>
2024-06-28 20:49:55 -0700

    Added to the resume section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,7 +1,118 @@
 export default function Experience() {
   return (
-    <div>
-      <h3>EXPERIENCES</h3>
-    </div>
+    <>
+      <div>
+        <h3>EXPERIENCES</h3>
+      </div>
+      <div>
+        <h4>Chief Information Security Officer, Software Engineer</h4>
+        <p>January 2023 - June 2024</p>
+        <h4>Healthcare Download, Remote Az</h4>
+        <p>
+          Software engineer and the first full-time employee hired by the
+          company. I've played a pivotal role in developing innovative solutions
+          for the healthcare analytics company. Key achievements include:
+        </p>
+        <ul>
+          <li>
+            As chief information security officer I created a secure site
+            through end to end encryption while ensuring the company followed
+            HIPPA laws and other healthcare regulations.
+          </li>
+          <li>
+            Spearheaded the development of a cutting-edge broker website,
+            facilitating seamless health insurance transitions for users.
+          </li>
+          <li>
+            Designed, programmed, and rigorously tested the user dashboard,
+            providing users with easy access to their healthcare information.
+          </li>
+          <li>
+            Recognized with the 'Rockstar Award' for pioneering a secure fetch
+            call mechanism for establishing a secure connection with healthcare
+            insurance companies.
+          </li>
+          <li>
+            Played a pivotal role in presentations to venture capitalists,
+            showcasing the web application's potential for disruption in the
+            health analytics sector.
+          </li>
+          <li>
+            Proficiently developed both backend and frontend components of the
+            website using technologies such as React, Node.js, Postman, and
+            JavaScript.
+          </li>
+          <li>
+            Communicated daily progress of widget creation and server status to
+            necessary personnel in the company.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Sales Associate</h4>
+        <p>May 2022 - August 2022</p>
+        <h4>Best Buy Outlet Center, Phoenix, Az</h4>
+        <p>
+          As a Sales Associate, I effectively communicated the features and
+          benefits of major appliances to customers, playing a crucial role in
+          facilitating sales. My responsibilities included managing the
+          efficient movement of products from storage to the sales floor,
+          ensuring optimal product availability and presentation. Notable
+          achievements during my tenure include:
+        </p>
+        <ul>
+          <li>
+            Generating $100,000 in product sales within the first month of the
+            store's opening, showcasing my ability to quickly establish rapport
+            with customers and drive revenue.
+          </li>
+          <li>
+            Maintaining a top-ranking position as the number one salesperson on
+            the sales team for three consecutive months, a testament to my sales
+            acumen, product knowledge, and customer-focused approach.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Middle School Science Teacher</h4>
+        <p>August 2020 - May 2022 </p>
+        <h4>Pendergast Elementary School District, Phoenix, Az</h4>
+        <p>
+          As an educator, I played a multifaceted role in fostering an enriching
+          learning environment. My contributions included:
+        </p>
+        <ul>
+          <li>
+            Devised a comprehensive curriculum that not only guided students'
+            learning but also encouraged their active engagement in the
+            educational process. I also took a proactive approach in managing
+            interpersonal relationships among students, ensuring a harmonious
+            and supportive classroom atmosphere.
+          </li>
+          <li>
+            Collaborated closely with the teaching team to develop strategic
+            plans aimed at maximizing student success. These collaborative
+            efforts helped in tailoring the curriculum and teaching methods to
+            the unique needs of our student body. Achieved a remarkable 25%
+            increase in student comprehension of science through the innovative
+            incorporation of teamwork projects into the curriculum. This
+            approach not only enhanced their understanding but also honed their
+            collaborative skills, essential for real-world success.
+          </li>
+          <li>
+            Pioneered the introduction of a 3D printing program for students,
+            which led to our middle school becoming the most active 3D printing
+            studio in the country. This initiative empowered students with
+            practical skills and exposed them to cutting-edge technology,
+            setting new standards for educational programs nationwide.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Sixth Grade Science Student Teacher</h4>
+        <p>December 2019 - May 2020</p>
+        <h4>Deer Valley Unified School District, Phoenix, Az</h4>
+      </div>
+    </>
   );
 }
