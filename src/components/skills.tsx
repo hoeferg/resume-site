4528507bc6e0d93ce04ff55ae878f4ec9e360d64
Gayle Hoefer <gaylehoefer@gmail.com>
2024-06-30 18:51:56 -0700

    Added skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,6 +2,11 @@ export default function Skills() {
   return (
     <div>
       <h3>SKILLS & PROFICIENCY</h3>
+      <h4>Node.js</h4>
+      <h4>React.js</h4>
+      <h4>Java</h4>
+      <h4>Python</h4>
+      <h4>Sequelize</h4>
     </div>
   );
 }
