5c42f667917788c577bf2e906ab6bbf9e5da832f
Gayle Hoefer <gaylehoefer@gmail.com>
2024-07-23 13:41:40 -0700

    Added stylings to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import MyInfo from "./parts/myinfo";
 import Body from "./parts/body";
-import "./index.css"
+import "./index.css";
 
 function App() {
   return (
-    <div className="p-10 items-center text-center grid grid-rows-1">
-      <MyInfo />
-      <Body />
+    <div className="bg-gray">
+      <div className="m-10 items-start text-center flex border-2 border-black bg-white">
+        <div className="">
+          <Body />
+        </div>
+        <div className="h-max ml-5 mr-5" >
+          <MyInfo />
+        </div>
+      </div>
     </div>
   );
 }
